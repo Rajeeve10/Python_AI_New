2f859bf57dd120f6bb716ca84153ee825fe353ca
Rajeeve Abraham <rajeeve10@gmail.com>
2026-08-25 12:24:00 +0100

    re factored the code

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,4 +1,6 @@
-firstNumber=10
-secondNumber=20
+#ajust add a comment to check refactoring
+#modifying the code 
+firstNumber=input("Enter your first number ")
+secondNumber=input("Enter your Second number ")
 result=firstNumber + secondNumber
 print(result)
